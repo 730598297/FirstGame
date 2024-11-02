b3c51810d348496655e4986b53aaffb4ef40b1d9
Alex <1alexasalinas@gmail.com>
2024-11-02 16:48:28 -0400

    Start of a new enemy existance Goblin

--- hacknc/scripts/player.gd
+++ hacknc/scripts/player.gd
@@ -2,19 +2,22 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+@onready var flip = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if "ui_left":
+		#flip.flip_h = true
 		velocity.x = direction * SPEED
-	else:
+	elif "ui_right":
 		velocity.x = direction * SPEED
+		#flip.flip_h = false
 		
 
 	var yAxis := Input.get_axis("ui_up", "ui_down")
