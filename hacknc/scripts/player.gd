cef08452dd9f312b87d89fcf82fb8f2647e8139f
Alex <1alexasalinas@gmail.com>
2024-11-02 14:38:56 -0400

    Working Base Player Model with camera moving with it and new files

---
+++ hacknc/scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = direction * SPEED
+		
+
+	var yAxis := Input.get_axis("ui_up", "ui_down")
+	if yAxis:
+		velocity.y = yAxis * SPEED
+	else:
+		velocity.y = yAxis * SPEED
+
+	move_and_slide()
